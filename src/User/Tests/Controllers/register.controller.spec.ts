3207da0fa8cd141627b39e3a8f4d716b88e30944
gabriel <gabriel.slawski@gmail.com>
2021-02-13 17:39:36 +0100

    register controller test updated on sw hero

--- src/User/Tests/Controllers/register.controller.spec.ts
+++ src/User/Tests/Controllers/register.controller.spec.ts
@@ -3,6 +3,7 @@ import { RegisterController } from '../../Controllers/register.controller';
 import { PasswordService } from '../../Services/password.service';
 import { RegisterDTO } from '../../DTO/register.dto';
 import { CreateUserService } from '../../Services/create-user.service';
+import { HeroService } from '../../../SWResource/Services/hero.service';
 
 const createUserServiceMock = () => ({
   create: jest.fn(),
@@ -12,9 +13,14 @@ const passwordServiceMock = () => ({
   hash: jest.fn(),
 });
 
+const heroServiceMock = () => ({
+  getRandomHeroUrl: jest.fn(),
+});
+
 describe('RegisterController', () => {
   let createUserService: CreateUserService,
     passwordService: PasswordService,
+    heroService: HeroService,
     controller: RegisterController;
 
   beforeEach(async () => {
@@ -22,12 +28,14 @@ describe('RegisterController', () => {
       providers: [
         { provide: CreateUserService, useFactory: createUserServiceMock },
         { provide: PasswordService, useFactory: passwordServiceMock },
+        { provide: HeroService, useFactory: heroServiceMock },
       ],
     }).compile();
 
     createUserService = await module.resolve(CreateUserService);
+    heroService = await module.resolve(HeroService);
     passwordService = await module.resolve(PasswordService);
-    controller = new RegisterController(createUserService, passwordService);
+    controller = new RegisterController(createUserService, passwordService, heroService);
   });
 
   describe('register', () => {
@@ -41,10 +49,19 @@ describe('RegisterController', () => {
       expect(passwordService.hash).toHaveBeenCalledWith(dto.password);
     });
 
+    it('uses hero service to obtain sw hero for new user', async () => {
+      jest.spyOn(heroService, 'getRandomHeroUrl').mockResolvedValue('http://url');
+
+      await controller.register(dto);
+
+      expect(heroService.getRandomHeroUrl).toHaveBeenCalled();
+    });
+
     it('uses create user service to create user with given data', async () => {
       jest.spyOn(passwordService, 'hash').mockResolvedValue('hashed');
+      jest.spyOn(heroService, 'getRandomHeroUrl').mockResolvedValue('http://url');
 
-      const partial = { ...dto, password: 'hashed' };
+      const partial = { ...dto, password: 'hashed', swHero: 'http://url' };
 
       await controller.register(dto);
 
